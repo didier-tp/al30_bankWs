ddc8a9b73ccb52a80c3ff9004e657c549a47b9a8
didier <didier@iciOuLa.fr>
2017-12-04 16:56:30 +0100

    nouvelle version

--- init_push.bat
+++ init_push.bat
@@ -2,7 +2,7 @@ cd /d "%~dp0"
 git init
 git add *
 git commit -a -m "version initiale"
-git remote add gitHubOrigin_al30_bankWs https://didier-tp:pwd007!@github.com/didier-tp/al30_bankWs.git
+git remote add gitHubOrigin_al30_bankWs https://didier-tp:....!@github.com/didier-tp/al30_bankWs.git
 git push -u gitHubOrigin_al30_bankWs master
 pause
 
